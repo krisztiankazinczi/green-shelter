e0c417127e0cadbeb3c8add96d6ebc2d8dae9672
Krisztian <krisztian.kazinczi@gmail.com>
2021-03-23 20:27:13 +0100

    role_id added in users table with default value

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at'
     ];
 
     /**
@@ -40,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->hasOne(Role::class);
+    }
 }
