f6fc171becc86493c574a88cfbea0c8ffbdd4e81
Krisztian <krisztian.kazinczi@gmail.com>
2021-03-30 21:28:11 +0200

    fontawesome installed and create button created on list pages

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,6 +7,7 @@
       <!-- Fonts -->
       <link rel="dns-prefetch" href="//fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+      <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
       <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
