33c46a63f561bdbddfc4966c55d436ef1ea9e96a
Krisztian <krisztian.kazinczi@gmail.com>
2021-05-26 23:03:34 +0200

    strange index primary keys problem in clearDB - solution try

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -14,124 +14,129 @@ class MenuSeeder extends Seeder
      */
     public function run()
     {
+
+        $role1 = DB::table('roles')->where('name', '=', 'Guest')->select('id')->first();
+        $role2 = DB::table('roles')->where('name', '=', 'Registered')->select('id')->first();
+        $role3 = DB::table('roles')->where('name', '=', 'Admin')->select('id')->first();
+
         $menuOptions = [
             [
                 'route' => 'home',
                 'name' => 'Kezdőlap',
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'animals',
                 'name' => 'Gazdira váró állatok',
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'dogs',
                 'name' => 'Gazdira váró kutyák',
                 'parent' => 2,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'dogs/create',
                 'name' => 'Gazdira váró kutyák regisztrálása',
                 'parent' => 2,
-                'role_id' => '3',
+                'role_id' => $role3->id,
             ],
             [
                 'route' => 'cats',
                 'name' => 'Gazdira váró macskák',
                 'parent' => 2,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'cats/create',
                 'name' => 'Gazdira váró macskák regisztrálása',
                 'parent' => 2,
-                'role_id' => '3',
+                'role_id' => $role3->id,
             ],
             [
                 'route' => 'lost-dogs',
                 'name' => 'Elveszett kutyák',
                 'parent' => 2,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'lost-dogs/create',
                 'name' => 'Elveszett kutyák regisztrálása',
                 'parent' => 2,
-                'role_id' => '2',
+                'role_id' => $role2->id,
             ],
             [
                 'route' => 'found-dogs',
                 'name' => 'Talált kutyák',
                 'parent' => 2,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'found-dogs/create',
                 'name' => 'Talált kutyák Regisztrálása',
                 'parent' => 2,
-                'role_id' => '2',
+                'role_id' => $role2->id,
             ],
             [
                 'route' => 'advertisements',
                 'name' => 'Lakossági hirdetések',
                 'parent' => 2,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'advertisements/create',
                 'name' => 'Lakossági hirdetések létrehozása',
                 'parent' => 2,
-                'role_id' => '2',
+                'role_id' => $role2->id,
             ],
             [
                 'route' => 'animal-of-week',
                 'name' => 'A hét állata',
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'success-stories',
                 'name' => 'Sikertörténetek',
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'gallery',
                 'name' => 'Képgaléria',
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'stories',
                 'name' => 'Írásaink',
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'about-us',
                 'name' => 'Rólunk',
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => '',
                 'name' => 'Rólunk',
                 'parent' => 17,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'contact-details',
                 'name' => 'Kapcsolat',
                 'parent' => 17,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'reviews',
                 'name' => 'Rólunk mondták',
                 'parent' => 17,
-                'role_id' => '1',
+                'role_id' => $role1->id,
             ],
             [
                 'route' => 'admin-dashboard/adoption/requested/all/7',
                 'name' => 'Admin Felület',
-                'role_id' => '3',
+                'role_id' => $role3->id,
             ],
         ];
         
